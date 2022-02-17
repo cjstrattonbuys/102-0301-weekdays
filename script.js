e7fd52c62b54c1cb4e5f692ebd089fc5ea6da2ac
CJ Buys <cjstrattonbuys@gmail.com>
2022-02-17 09:56:57 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+//var weekday = new Date().toLocaleString("en-US", { weekday: "long" });
+
+// console.log(weekday);
+// Your code here
+var weekday = "Sunday";
+
+if (weekday === "Monday") {
+  console.log("Let’s get this week started!");
+} else if (weekday === "Tuesday") {
+  console.log("Worky work!");
+} else if (weekday === "Wednesday") {
+  console.log("Halfway there!");
+} else if (weekday === "Thursday") {
+  console.log("Friday lite!");
+} else if (weekday === "Friday") {
+  console.log("Weekend’s almost here!");
+} else {
+  console.log("Yay, it's the weekend!");
+}
